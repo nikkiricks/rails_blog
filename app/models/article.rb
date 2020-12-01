908cd8bff97c77ab05db363414d15f022cd038a7
Nikki Ricks <nikki.ricks@envato.com>
2020-12-01 11:32:36 +1100

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,4 +12,7 @@ class Article < ApplicationRecord
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
